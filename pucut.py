b6ea174faa848f092d90415c453db2dada03ce1e
Etilia01 <alexandridisjulia@gmail.com>
2026-07-18 15:53:48 +0200

    ADDED CUSTOM FONTS (why does tkinter make that near impossible??? Should have used literally anything else)

--- pucut.py
+++ pucut.py
@@ -1,7 +1,10 @@
 #colorscheme: 574964, 9F8383, C8AAAA, FFDAB3
 # basics almost done, to-do for tmmrw: longer happyiness cycles, styling, maybe a mini-game for playing?
+import ctypes
+import os
 import pygame
 from tkinter import *
+from tkinter import font as f
 import tkinter.messagebox
 from tkinter import filedialog as fd
 bg_color = "#574964"
@@ -9,7 +12,7 @@ font_color= "#FFDAB3"
 happyness= 10
 tired= 10
 window = Tk()
-window.geometry("380x500")
+window.geometry("380x520")
 window.title("Pucut <3")
 icon_image = tkinter.PhotoImage(file="images/icon.png")
 window.iconphoto(False, icon_image)
@@ -29,11 +32,32 @@ musictone = "happy"
 petname= "none"
 happynesscycle= True
 petzoomed= pet.zoom(4)
+playbutton= PhotoImage(file="images/play.png")
+bigplaybutton= playbutton.zoom(2)
+foodbutton= PhotoImage(file="images/food.png")
+bigfoodbutton= foodbutton.zoom(2)
+sleepbutton= PhotoImage(file="images/sleep.png")
+bigsleepbutton= sleepbutton.zoom(2)
 petexpressions=["images/virtual pet 1.png", "images/virtual pet happy.png", "images/virtual pet sad.png", "images/virtual pet sleeping.png", "images/virtual pet tired.png", "images/virtual pet very happy.png", "images/virtual pet 2.png"]
 pygame.init()
 pygame.mixer.music.load("music/happy song.mp3")
 pygame.mixer.music.play()
 nameset = tkinter.StringVar()
+fontpath1 = os.path.abspath("fonts/Kablammo/Kablammo-Regular-VariableFont_MORF.ttf")
+fontpath2 = os.path.abspath("fonts/Rubik_Glitch/RubikGlitch-Regular.ttf")
+
+
+try:
+    libfc = ctypes.CDLL("libfontconfig.so.1")
+    ttf1 = fontpath2.encode('utf-8')
+    ttf2 = fontpath1.encode('utf-8')
+    libfc.FcConfigAppFontAddFile(None, ttf1)
+    libfc.FcConfigAppFontAddFile(None, ttf2)
+except Exception as e:
+    print(f"Font couldnt be loaded cuz {e}")
+font1 = f.Font(family="Rubik Glitch", size=10)
+font2 = f.Font(family="Kablammo", size=10)
+
 edge1 = tkinter.Label(window, image=edgedecor1, background= bg_color)
 edge1.place(relx=0.0, rely=0.0, anchor=tkinter.NW)
 edge2 = tkinter.Label(window, image=edgedecor2big, background= bg_color)
@@ -211,37 +235,56 @@ buttonframe.pack(pady=1, padx=1)
 
 food_button = tkinter.Button(
     buttonframe,
-    width=10, 
-    height=2,  
-    padx=2,         
-    pady=2,             
+    width=60, 
+    height=60,  
+    padx=4,         
+    pady=4,             
     command=food,
-    text= "food"
+    image= bigfoodbutton,
+    text= "food",
+    bg= "#9F8383",
+    activebackground= "#C8AAAA",
+    bd= 2,
+    fg= font_color,
+    highlightthickness=0
 )
 food_button.pack(side=LEFT, padx=3)
 sleep_button = tkinter.Button(
     buttonframe,
-    width=10, 
-    height=2,  
-    padx=2,         
-    pady=2,             
+    width=60, 
+    height=60,  
+    padx=4,         
+    pady=4,             
     command=sleep,
-    text = "sleep"
+    image= bigsleepbutton,
+    text = "sleep",
+    bg= "#9F8383",
+    activebackground= "#C8AAAA",
+    bd= 2,
+    fg= font_color,
+    highlightthickness=0
 )
 sleep_button.pack(side=LEFT, padx=3)
 play_button = tkinter.Button(
     buttonframe,
-    width=10, 
-    height=2,  
-    padx=2,         
-    pady=2,             
+    width=60, 
+    height=60,  
+    padx=4,         
+    pady=4,             
     command=play,
-    text= "play"
+    text= "play",
+    image= bigplaybutton,
+    bg= "#9F8383",
+    activebackground= "#C8AAAA",
+    bd= 2,
+    fg= font_color,
+    highlightthickness=0
 )
 play_button.pack(side=LEFT, padx=3)
 pucut = tkinter.Label(mainframe, image=petzoomed, background= bg_color)
 pucut.pack(anchor=S, pady=5)
-desc= tkinter.Label(mainframe, background= bg_color, text= petname + " is happy")
+desc= tkinter.Label(mainframe, background= bg_color, text= petname + " is happy", font= font2
+)
 desc.pack(anchor=S, pady=3)
 settings_button = tkinter.Button(
     mainframe,
